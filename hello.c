b5cdbf5eb894c71670e16a648aa2984821b0c6f9
Mayank Sharma <msharma6@huskers.unl.edu>
2023-09-01 17:43:34 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    char *name = "Mayank Sharma";
+    char *name = "Computer Science";
+
+    printf("Hello, %s here.\n", name);
+    printf("My major is, %s\n", major);
+
+    return 0;
+}
